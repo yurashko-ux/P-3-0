3bd3efd8cf93e177d16be7e84776f83679ce545e
yurashko-ux <yurashko@gmail.com>
2025-09-04 11:28:45 +0300

    Create page.tsx

---
+++ app/admin/playground/page.tsx
@@ -0,0 +1,8 @@
+export default function PlaygroundPage() {
+  return (
+    <main data-admin className="admin-shell">
+      <h1>Playground</h1>
+      <p>Заглушка. Тут буде тестовий майданчик.</p>
+    </main>
+  );
+}
