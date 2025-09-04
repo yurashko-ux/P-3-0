fcdf1da2faf72f4186ed5128651bc718dacbcb18
yurashko-ux <yurashko@gmail.com>
2025-09-04 11:28:29 +0300

    Create page.tsx

---
+++ app/admin/payloads/page.tsx
@@ -0,0 +1,8 @@
+export default function PayloadsPage() {
+  return (
+    <main data-admin className="admin-shell">
+      <h1>Payloads</h1>
+      <p>Заглушка. Тут можна буде переглядати payloadʼи.</p>
+    </main>
+  );
+}
