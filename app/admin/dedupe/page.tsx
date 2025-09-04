6d02d2d71f862a4db25b55d128e8e5e7474f9425
yurashko-ux <yurashko@gmail.com>
2025-09-04 11:29:11 +0300

    Create page.tsx

---
+++ app/admin/dedupe/page.tsx
@@ -0,0 +1,8 @@
+export default function DedupePage() {
+  return (
+    <main data-admin className="admin-shell">
+      <h1>Dedupe</h1>
+      <p>Заглушка. Тут буде розділ з дедуплікацією.</p>
+    </main>
+  );
+}
