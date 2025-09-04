a97b7e6b3440d452b4d3ea462b727c6f88d57de1
yurashko-ux <yurashko@gmail.com>
2025-09-04 11:27:58 +0300

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminHome() {
+  return (
+    <main data-admin className="admin-shell">
+      <h1>Адмінка</h1>
+      <p>Заглушка сторінки. Тут зʼявиться панель адміністратора.</p>
+    </main>
+  );
+}
