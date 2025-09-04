e1b0fd6deeef252da19bf92be90a3cd4d9b73f3f
yurashko-ux <yurashko@gmail.com>
2025-09-04 11:28:59 +0300

    Create page.tsx

---
+++ app/admin/mappings/page.tsx
@@ -0,0 +1,8 @@
+export default function MappingsPage() {
+  return (
+    <main data-admin className="admin-shell">
+      <h1>Mappings</h1>
+      <p>Заглушка. Тут будуть відповідності/мапінги.</p>
+    </main>
+  );
+}
