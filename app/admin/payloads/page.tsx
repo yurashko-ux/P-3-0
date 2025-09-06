d222f3e46f2563075fb664160148fb494a640f50
yurashko-ux <yurashko@gmail.com>
2025-09-06 10:47:12 +0300

    Delete app directory

--- app/admin/payloads/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PayloadsPage() {
-  return (
-    <main data-admin className="admin-shell">
-      <h1>Payloads</h1>
-      <p>Заглушка. Тут можна буде переглядати payloadʼи.</p>
-    </main>
-  );
-}
