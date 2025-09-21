85c1b3212534e92064e5a121d981e38caa7e9825
yurashko-ux <yurashko@gmail.com>
2025-09-22 00:43:55 +0300

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,11 +1,14 @@
 // web/next.config.js
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  experimental: {
-    // Force Next to honor tsconfig "paths" -> "@/..." imports
-    tsconfigPaths: true,
+  // remove invalid experimental.tsconfigPaths
+  webpack: (config) => {
+    // Map "@/..." to the web/ directory explicitly
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
 module.exports = nextConfig;
-
