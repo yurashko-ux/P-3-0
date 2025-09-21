caf3e7cf054d9c6f207941460043f839c74f72c5
yurashko-ux <yurashko@gmail.com>
2025-09-22 00:28:38 +0300

    Create next.config.js

---
+++ web/next.config.js
@@ -0,0 +1,10 @@
+// web/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // Force Next to honor tsconfig "paths" -> "@/..." imports
+    tsconfigPaths: true,
+  },
+};
+
+module.exports = nextConfig;
