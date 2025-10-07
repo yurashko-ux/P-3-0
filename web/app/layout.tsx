5be9d5554447f2c55a2912a0f052054b1b20a6fe
yurashko-ux <yurashko@gmail.com>
2025-10-08 01:26:49 +0300

    Guard client against SES lockdown failures

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -22,12 +22,54 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
            Виконується ПЕРЕД усіма іншими скриптами. */}
         <Script id="disable-ses-lockdown" strategy="beforeInteractive">
           {`
-            try {
-              var g = (typeof globalThis !== 'undefined' ? globalThis : window);
-              if (g && typeof g.lockdown === 'function') {
-                try { delete g.lockdown; } catch (_) { try { g.lockdown = undefined; } catch (_) {} }
-              }
-            } catch (_) {}
+            (function(){
+              try {
+                var g = typeof globalThis !== 'undefined' ? globalThis : window;
+                if (!g) return;
+                var w = typeof window !== 'undefined' ? window : g;
+                var noop = function lockdownDisabled() { return undefined; };
+                var install = function(target) {
+                  try {
+                    Object.defineProperty(target, 'lockdown', {
+                      configurable: true,
+                      writable: true,
+                      value: noop,
+                    });
+                  } catch (_) {
+                    try {
+                      target.lockdown = noop;
+                    } catch (_) {}
+                  }
+                };
+                install(g);
+                try {
+                  var originalDefineProperty = Object.defineProperty;
+                  Object.defineProperty = function(target, property, descriptor) {
+                    if ((target === g || target === w) && property === 'lockdown') {
+                      return originalDefineProperty(target, property, {
+                        configurable: true,
+                        writable: true,
+                        value: noop,
+                      });
+                    }
+                    return originalDefineProperty(target, property, descriptor);
+                  };
+                } catch (_) {}
+                try {
+                  var originalReflectDefine = Reflect.defineProperty;
+                  Reflect.defineProperty = function(target, property, descriptor) {
+                    if ((target === g || target === w) && property === 'lockdown') {
+                      return originalReflectDefine(target, property, {
+                        configurable: true,
+                        writable: true,
+                        value: noop,
+                      });
+                    }
+                    return originalReflectDefine(target, property, descriptor);
+                  };
+                } catch (_) {}
+              } catch (_) {}
+            })();
           `}
         </Script>
       </head>
