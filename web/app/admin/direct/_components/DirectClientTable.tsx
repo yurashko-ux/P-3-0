ba28cd8952c3e3c594987b33ce0959cf2b6b917d
Mykolay <mykolay@users.noreply.github.com>
2026-01-20 02:00:59 +0200

    Set full name column width to 180px

--- web/app/admin/direct/_components/DirectClientTable.tsx
+++ web/app/admin/direct/_components/DirectClientTable.tsx
@@ -987,7 +987,7 @@ export function DirectClientTable({
                 <col style={{ width: 60 }} />
                 <col style={{ width: 44 }} />
                 {/* Повне імʼя (суттєво ширше, щоб менше обрізалось) */}
-                <col style={{ width: 200 }} />
+                <col style={{ width: 180 }} />
               </colgroup>
               <thead>
                 <tr className="bg-base-200">
