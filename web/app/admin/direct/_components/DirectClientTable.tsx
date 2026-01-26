a1eb4d719ed6e6c942215da5c3489b5ae65dc4b0
Mykolay <mykolay@users.noreply.github.com>
2026-01-26 21:26:36 +0200

    🔧 Змінено позиціонування футера на bottom-[-6px] для закриття білої полоси знизу

--- web/app/admin/direct/_components/DirectClientTable.tsx
+++ web/app/admin/direct/_components/DirectClientTable.tsx
@@ -2477,7 +2477,7 @@ export function DirectClientTable({
       </div>
       
       {/* Футер після таблиці */}
-      <div className="absolute bottom-0 left-0 right-0 bg-gray-200 min-h-[250px] p-4 -mx-4 w-[calc(100%+2rem)]">
+      <div className="absolute bottom-[-6px] left-0 right-0 bg-gray-200 min-h-[250px] p-4 -mx-4 w-[calc(100%+2rem)]">
         {/* Тут можна додавати дані */}
       </div>
     </div>
